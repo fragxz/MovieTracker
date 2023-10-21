52e139596a60c754ba63467ba667aa4dc37bb046
Frederic Reinhardt <10348589+fragxz@users.noreply.github.com>
2023-10-21 22:38:15 +0200

    Added logo and favicon

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        <img src="{{ asset('logo.png') }}" alt="MovieTracker Logo" style="max-width: 50px;">
                     </a>
                 </div>
 
